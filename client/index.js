7c4a9c7e5852f942710497e34a76e3430bc87782
Ragnar Kalland <ragnar.kalland@gmail.com>
2014-09-21 19:14:48 +0200

    Div templates and css

--- client/index.js
+++ client/index.js
@@ -13,8 +13,25 @@
 	Router.map(function() {
 		this.route('start_page', {path: '/'});
 		this.route('about', {path: '/about'});
-		
+		this.route('registration');
+		this.route('R1Step1');
+		this.route('R1Step2');
+		this.route('R2Step1');
+        this.route('map');
+        this.route('seat');
 		});
+
+    Template.map.seats = [
+        {status: "available"},
+        {status: "unavailable"},
+        {status: "available"},
+        {status: "available"},
+        {status: "available"},
+        {status: "available"},
+        {status: "available"},
+        {status: "available"}
+    ];
+    Template.map.rows = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 
 }(Meteor));
